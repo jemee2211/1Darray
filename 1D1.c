6fb7b39442741ce1ae851faeadef20ad886ee9f1
jemee2211 <120568492+jemee2211@users.noreply.github.com>
2022-12-27 17:04:25 +0530

    Add files via upload

---
+++ 1D1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(){
+	
+	int n;
+	
+	printf("Enter Number: ");
+	scanf("%d",&n);
+	
+	int i;
+	int a[n] ;
+	for(i=0; i<n; i++){
+		
+		scanf("%d",&a[i]);
+	}
+	for(i=0; i<n; i++){
+		
+		printf("%d\n",a[i]);
+	}
+	
+	
+	
+}
---
+++ 1D4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main(){
+	
+	int n;
+	
+	printf("Enter Number: ");
+	scanf("%d",&n);
+	
+	int i;
+	int a[n] ;
+	int sum=0;
+	
+	for(i=0; i<n; i++){
+		
+			printf("Enter value: ");
+		scanf("%d",&a[n]);
+		
+		sum=sum+a[n];
+	}
+	
+	
+	printf("%d",sum);
+	
+	
+	
+
+}
